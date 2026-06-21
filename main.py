d74158dc16cabdddc7d181c5502c31f799f02a1d
FaiqRN <faiqramzy2004@gmail.com>
2026-06-22 00:54:29 +0700

    p

--- main.py
+++ main.py
@@ -147,22 +147,22 @@ tiles.set_tilemap(tilemap("""
     level1
     """))
 mySprite = sprites.create(img("""
-        . . . . f f f f . . . . .
-        . . f f f f f f f f . . .
-        . f f f f f f c f f f . .
-        f f f f f f c c f f f c .
-        f f f c f f f f f f f c .
-        c c c f f f e e f f c c .
-        f f f f f e e f f c c f .
-        f f f b f e e f b f f f .
-        . f 4 1 f 4 4 f 1 4 f . .
-        . f e 4 4 4 4 4 4 e f . .
-        . f f f e e e e f f f . .
-        f e f 5 8 8 8 8 5 f e f .
-        e 4 f 8 8 8 8 8 8 f 4 e .
-        e e f 9 9 9 9 9 9 f e e .
-        . . . f f f f f f . . . .
-        . . . f f . . f f . . . .
+        . . . . . . 5 . 5 . . . . . . .
+        . . . . . f 5 5 5 f f . . . . .
+        . . . . f 1 5 2 5 1 6 f . . . .
+        . . . f 1 6 6 6 6 6 1 6 f . . .
+        . . . f 6 6 f f f f 6 1 f . . .
+        . . . f 6 f f d d f f 6 f . . .
+        . . f 6 f d f d d f d f 6 f . .
+        . . f 6 f d 3 d d 3 d f 6 f . .
+        . . f 6 6 f d d d d f 6 6 f . .
+        . f 6 6 f 3 f f f f 3 f 6 6 f .
+        . . f f d 3 5 3 3 5 3 d f f . .
+        . . f d d f 3 5 5 3 f d d f . .
+        . . . f f 3 3 3 3 3 3 f f . . .
+        . . . f 3 3 5 3 3 5 3 3 f . . .
+        . . . f f f f f f f f f f . . .
+        . . . . . f f . . f f . . . . .
         """),
     SpriteKind.player)
 mySprite2 = sprites.create(img("""
